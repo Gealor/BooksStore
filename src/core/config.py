da5d62428df74ffc87abd71d86e10b2c6c1029b3
Gealor <gzazulya@gmail.com>
2025-06-08 22:32:14 +0400

    added database parameters in config.py

--- src/core/config.py
+++ src/core/config.py
@@ -1,4 +1,4 @@
-from fastapi import Path
+from pathlib import Path
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -11,6 +11,29 @@ class RunConfig(BaseModel):
     host : str = '0.0.0.0'
     port : str = 8000
 
+class DatabaseConfig(BaseModel):
+    user: str
+    password: str
+    host: str
+    port: str
+    name: str
+
+    echo : bool
+    echo_pool:  bool 
+    pool_size: int
+    max_overflow: int
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s"
+    } # чтобы имена ограничений также учитывались alembic миграциями
+
+    def get_db_url(self):
+        return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.name}"
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file = (ENV_TEMPLATE_FILE, ENV_FILE),
@@ -20,5 +43,8 @@ class Settings(BaseSettings):
     )
 
     run : RunConfig = RunConfig()
+    db : DatabaseConfig 
+
+
 
 settings = Settings()
