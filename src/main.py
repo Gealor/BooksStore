a5d0444e119e937533e7fd6399a3c7131f1131a0
Gealor <gzazulya@gmail.com>
2025-06-08 22:57:59 +0400

    connect main_router to main_app

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
+import datetime
 from fastapi import Depends, FastAPI, Request, Response
 import uvicorn
 
 from core.config import settings
+from api import router as main_router
 
 main_app = FastAPI()
+
+main_app.include_router(
+    main_router,
+)
 
 if __name__=="__main__":
     uvicorn.run(
