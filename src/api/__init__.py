fd489a0cc1b2eb7b592274fdf44e1a3ae57bd4ec
Gealor <gzazulya@gmail.com>
2025-06-09 09:38:05 +0400

    connect routers to main router

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,7 +1,17 @@
 from fastapi import APIRouter
 
 from core.config import settings
+from .auth.jwt_auth import router as auth_router
+from .users.views import router as user_router
 
 router = APIRouter(
     prefix = settings.api.prefix,
+)
+
+router.include_router(
+    auth_router
+)
+
+router.include_router(
+    user_router
 )
