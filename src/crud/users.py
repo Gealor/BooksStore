bc52cafa570c0a2774736a42e5591241a9fe78b8
Gealor <gzazulya@gmail.com>
2025-06-09 09:34:58 +0400

    added crud operations for users

--- src/crud/users.py
+++ src/crud/users.py
@@ -1,60 +1,62 @@
 from typing import Sequence
 from sqlalchemy import select
-from sqlalchemy.orm import selectinload
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload, Session
 
 from auth import tools as auth_tools
 from core.models import User, db_helper
-from core.schemas.user import UserCreate, UserNameMail, UserRead, UserUpdate
+from core.schemas.user import UserCreate, UserRead
 
 
-async def get_all_users(
-    session : AsyncSession,
+def get_all_users(
+    session : Session,
 ) -> Sequence[UserRead]:
     stmt = select(User).order_by(User.id)
 
-    result = await session.scalars(stmt)
+    result = session.scalars(stmt)
     return result.all()
 
-async def get_user_by_id(
+def get_user_by_id(
     user_id : int,
-    session : AsyncSession,
+    session : Session,
 ) -> UserRead | None:
     stmt = select(User).where(User.id == user_id)
-    result = await session.scalar(stmt)
+    result = session.scalar(stmt)
 
     return result
 
-async def create_user(
+def create_user(
     user_create : UserCreate,
-    session : AsyncSession,
+    session : Session,
 ) -> UserRead:
     user_create.password = auth_tools.hash_password(user_create.password).decode('utf-8')
     
     user = User(**user_create.model_dump())
     session.add(user)
-    await session.flush()
+    session.commit()
+    session.refresh(user)
     return user
 
-async def delete_user_by_id(
+def delete_user_by_id(
     user_id : int,
-    session : AsyncSession,
-) -> None:
+    session : Session,
+) -> int | None:
     stmt = select(User).options(selectinload(User.borrowed_books)).where(User.id == user_id)
-    result = await session.execute(stmt)
+    result = session.execute(stmt)
     user = result.scalar_one_or_none()
     if user:
-        await session.delete(user)
-        await session.flush()
+        session.delete(user)
+        session.commit()
+    return user.id if user else None
+    
 
-async def update_user_data(
+def update_user_data(
     user : User,
     new_data : dict,
-    session : AsyncSession,
+    session : Session,
 ) -> None:
     if "password" in new_data:
         new_data["password"] = auth_tools.hash_password(new_data['password']).decode('utf-8')
     for key, value in new_data.items():
         setattr(user, key, value)
         
-    await session.flush()
+    session.commit()
