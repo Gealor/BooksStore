c6924007efbfe9526182c650da4b11c0d5866d70
Gealor <gzazulya@gmail.com>
2025-06-09 12:11:13 +0400

    connect user router and book router

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -3,15 +3,20 @@ from fastapi import APIRouter
 from core.config import settings
 from .auth.jwt_auth import router as auth_router
 from .users.views import router as user_router
+from .books.views import router as book_router
 
 router = APIRouter(
     prefix = settings.api.prefix,
 )
 
 router.include_router(
-    auth_router
+    auth_router,
 )
 
 router.include_router(
-    user_router
+    user_router,
+)
+
+router.include_router(
+    book_router,
 )
