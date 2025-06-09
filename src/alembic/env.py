b7c1706e8c2fa0e9ea249bb5388009fee3e943cf
Gealor <gzazulya@gmail.com>
2025-06-09 07:26:25 +0400

    alembic configuring and first revision

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -1,16 +1,12 @@
-import asyncio
 from logging.config import fileConfig
 
+from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from sqlalchemy.engine import Connection
-from sqlalchemy.ext.asyncio import async_engine_from_config
-
 from alembic import context
 
-# таблицы, наследующие от Base, должны, быть обязательно импотрированы(неважно куда)
 from core.models import Base
-# явно импортировать все таблицы(важен лишь факт импорта), которые будут мигрироваться, чтобы они добавились в Base.metadata
 from core.config import settings
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -25,7 +21,6 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-# ВАЖНО УКАЗАТЬ, ЧТОБЫ alembic ЗНАЛ КАКИЕ ТАБЛИЦЫ ПОДТЯГИВАТЬ(указывается базовый класс таблицы, от которого наследуются все остальные)
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
@@ -47,52 +42,38 @@ def run_migrations_offline() -> None:
 
     """
     url = config.get_main_option("sqlalchemy.url")
-    
     context.configure(
         url=url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
-        compare_server_default=True, # нужно чтобы при изменении параметра server_default были видны изменения
+        compare_server_default=True,
     )
 
     with context.begin_transaction():
         context.run_migrations()
 
 
-def do_run_migrations(connection: Connection) -> None:
-    context.configure(
-        connection=connection, 
-        target_metadata=target_metadata,
-        compare_server_default=True, # нужно чтобы при изменении параметра server_default были видны изменения
-    )
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode.
 
-    with context.begin_transaction():
-        context.run_migrations()
-
-
-async def run_async_migrations() -> None:
-    """In this scenario we need to create an Engine
+    In this scenario we need to create an Engine
     and associate a connection with the context.
 
     """
-
-    connectable = async_engine_from_config(
+    connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
-    async with connectable.connect() as connection:
-        await connection.run_sync(do_run_migrations)
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection, target_metadata=target_metadata, compare_server_default=True,
+        )
 
-    await connectable.dispose()
-
-
-def run_migrations_online() -> None:
-    """Run migrations in 'online' mode."""
-
-    asyncio.run(run_async_migrations())
+        with context.begin_transaction():
+            context.run_migrations()
 
 
 if context.is_offline_mode():
