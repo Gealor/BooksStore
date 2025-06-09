9f946ee7b6db1f3fb2473453ad63c956679ca690
Gealor <gzazulya@gmail.com>
2025-06-09 09:34:29 +0400

    added auth read operations

---
+++ src/crud/auth.py
@@ -0,0 +1,26 @@
+from typing import Tuple
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from core.models import User, db_helper
+from core.schemas.user import UserLoginInfo
+
+def find_user_by_username(
+    username : str,
+    session : Session,
+) -> Tuple[str] | None:
+    stmt = select(User.username).where(User.name == username)
+
+    result = session.execute(stmt)
+
+    return result.first()
+
+
+def get_data_by_username(
+    username : str,
+) -> UserLoginInfo | None:
+    with db_helper.session_factory() as session:
+        stmt = select(User).where(User.name == username)
+        result = session.scalar(stmt)
+
+    return result
