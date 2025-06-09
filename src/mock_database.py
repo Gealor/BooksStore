6a75123f203b0e8051f6ba5b3235e7234ce41de3
Gealor <gzazulya@gmail.com>
2025-06-09 07:25:58 +0400

    script for create mock databases

--- src/mock_database.py
+++ src/mock_database.py
@@ -2,11 +2,10 @@ import asyncio
 from core.models.base import Base
 from core.models.db_helper import db_helper_mock
 
-async def create_all_mock_tables():
-    async with db_helper_mock.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+def create_all_mock_tables():
+    Base.metadata.create_all(bind=db_helper_mock.engine)
     print("Тестовые базы данных созданы")
 
 
 if __name__=="__main__":
-    asyncio.run(create_all_mock_tables())
+    create_all_mock_tables()
