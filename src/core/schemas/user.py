cb23ef41a03e5b49a557ba66e5e3c00ffac44892
Gealor <gzazulya@gmail.com>
2025-06-10 14:36:05 +0400

    added constraints for schemas

--- src/core/schemas/user.py
+++ src/core/schemas/user.py
@@ -1,18 +1,21 @@
 
-
 from typing import Optional
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 
+from core.config import settings
 
 class UserBase(BaseModel):
-    name : str
+    name : str = Field(min_length=2)
     email : EmailStr
 
 class UserRead(UserBase):
     id : int
 
 class UserCreate(UserBase):
-    password : str
+    password : str = Field(
+        min_length=settings.validation.min_len_password, 
+        max_length=settings.validation.max_len_password
+    )
 
 class UserLoginInfo(UserRead):
     password : str
@@ -20,7 +23,11 @@ class UserLoginInfo(UserRead):
 class UserUpdate(UserCreate):
     name : Optional[str] = None
     email : Optional[EmailStr] = None
-    password : Optional[str] = None
+    password : Optional[str] = Field(
+        default=None, 
+        min_length=settings.validation.min_len_password, 
+        max_length=settings.validation.max_len_password
+    )
 
 
 class UserDelete(BaseModel):
