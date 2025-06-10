143c50be842286d108f80774be420769f01f333b
Gealor <gzazulya@gmail.com>
2025-06-10 14:21:27 +0400

    little refactoring and added dependency for jwt auth

--- src/api/auth/tools/tools_auth.py
+++ src/api/auth/tools/tools_auth.py
@@ -1,17 +1,18 @@
 from fastapi import Depends, Form, HTTPException, status
-from fastapi.security import HTTPBasic, HTTPBearer, HTTPAuthorizationCredentials, OAuth2PasswordBearer
-from pydantic import EmailStr
+from fastapi.security import HTTPBearer, OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from jwt.exceptions import InvalidTokenError
 
+from api.auth.tools.creation_tokens import ACCESS_TOKEN_TYPE, REFRESH_TOKEN_TYPE, TOKEN_TYPE_FIELD
 from core.models import db_helper
 from core.schemas.user import UserRead
 from crud import auth as auth_crud
 from crud import users as users_crud
 from auth import tools as auth_tools
 
+# чтобы в документации появилось поле для ввода токена
+http_bearer = HTTPBearer(auto_error=False) # чтобы не выбрасывал ошибку автоматически
 
-security = HTTPBasic()
 oauth2_schema = OAuth2PasswordBearer(tokenUrl = '/api/auth/login')
 
 def validate_auth_user(
@@ -48,14 +49,37 @@ def get_jwt_token(
         )
     return payload
 
-def get_current_active_auth_user(
-    payload : dict = Depends(get_jwt_token),
-    session : Session = Depends(db_helper.session_getter),     
-) -> UserRead:
+
+def validate_token_type(payload : dict, token_type_ : str):
+    token_type : str = payload.get(TOKEN_TYPE_FIELD)
+    if token_type != token_type_:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid token type',
+        )
+    
+def get_user_by_token_type(payload, session) -> UserRead:
     id : int | None = int(payload.get('sub'))
     if not (user := users_crud.get_user_by_id(id, session)):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail = "Invalid token."
+            detail='Invalid token',
         )
     return user
+
+def get_current_active_auth_user(
+    payload : dict = Depends(get_jwt_token),
+    session : Session = Depends(db_helper.session_getter),    
+    dep = Depends(http_bearer), 
+) -> UserRead:
+    validate_token_type(payload, ACCESS_TOKEN_TYPE)
+    return get_user_by_token_type(payload, session)
+
+
+def get_current_active_auth_user_for_refresh(
+    payload : dict = Depends(get_jwt_token),
+    session : Session = Depends(db_helper.session_getter),  
+    dep = Depends(http_bearer),    
+) -> UserRead:
+    validate_token_type(payload, REFRESH_TOKEN_TYPE)
+    return get_user_by_token_type(payload, session)
