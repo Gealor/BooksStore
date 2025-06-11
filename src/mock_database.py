c2d320fa5825bcfaf81d29d73b6820ccd1f48321
Gealor <gzazulya@gmail.com>
2025-06-11 16:40:17 +0400

    added drop all mock tables

--- src/mock_database.py
+++ src/mock_database.py
@@ -2,10 +2,15 @@ import asyncio
 from core.models.base import Base
 from core.models.db_helper import db_helper_mock
 
+def drop_all_mock_table():
+    Base.metadata.drop_all(bind=db_helper_mock.engine)
+    print("Тестовые базы данных сброшены")
+
 def create_all_mock_tables():
     Base.metadata.create_all(bind=db_helper_mock.engine)
     print("Тестовые базы данных созданы")
 
 
 if __name__=="__main__":
+    drop_all_mock_table()
     create_all_mock_tables()
