018d0e331d4f109dde6fdbd5c5e222a45123fe75
Gealor <gzazulya@gmail.com>
2025-06-11 18:41:16 +0400

    added script for create main database

---
+++ src/create_main_database.py
@@ -0,0 +1,36 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+from core.models.base import Base
+from core.models.db_helper import db_helper
+from core.config import settings
+
+def create_database():
+    conn = psycopg2.connect(
+        # в dbname введите существующую базу данных в вашей СУБД, к которой можно подключиться
+        dbname = "postgres", 
+        user = settings.db.user, 
+        password = settings.db.password,
+        host = settings.db.host,
+    )
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cur = conn.cursor()
+    cur.execute(f"CREATE DATABASE {settings.db.name}")
+    print("База данных создана")
+    cur.close()
+    conn.close()
+    
+
+def drop_all_mock_table():
+    Base.metadata.drop_all(bind=db_helper.engine)
+    print("Тестовые таблицы сброшены")
+
+def create_all_mock_tables():
+    Base.metadata.create_all(bind=db_helper.engine)
+    print("Тестовые таблицы созданы")
+
+
+if __name__=="__main__":
+    create_database()
+    drop_all_mock_table()
+    create_all_mock_tables()
