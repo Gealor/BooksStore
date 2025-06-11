3488b6cd710b0bf8a891b329dc0dfce40474bd78
Gealor <gzazulya@gmail.com>
2025-06-11 13:11:34 +0400

    fix test for users crud

--- tests/test_users_crud.py
+++ tests/test_users_crud.py
@@ -2,24 +2,17 @@
 Тестирование crud операций для users
 Для корректного тестирования в терминале пропишите команду set PYTHONPATH=%CD%\\src !!!
 '''
+from pydantic import ValidationError
+import pytest
 from sqlalchemy import text
 
-from core.schemas.users import UserCreate
+from core.schemas.users import UserCreate, UserUpdate
 from crud import users as users_crud
 from core.models.db_helper import db_helper_mock
-
-def clear_table_users(session):
-    session.execute(text("TRUNCATE users CASCADE;"))
-    session.execute(text("ALTER SEQUENCE users_id_seq RESTART WITH 1;"))
-    session.commit()
-
-with db_helper_mock.session_factory() as session:
-    clear_table_users(session)
-
-
+        
 def test_create_user():
     user = UserCreate(
-        name = "John",
+        name = "Максим",
         email = "test1@mail.ru",
         password = "craft123",
     )
@@ -29,7 +22,7 @@ def test_create_user():
             session,
         )
 
-        assert result.name == "John"
+        assert result.name == "Максим"
         assert result.email == 'test1@mail.ru'
 
 def test_get_all_users():
@@ -40,21 +33,36 @@ def test_get_all_users():
 
 def test_update_user_by_id():
     with db_helper_mock.session_factory() as session:
-        user = users_crud.get_user_by_id(1, session)
+        user = users_crud.get_user_by_id(4, session)
 
         assert user is not None
 
-        new_data = {
-            "name" : "Gealor",
-            "email" : "test2@gmail.com",
-        }
+        user_data = UserUpdate(
+            name = "Gealor",
+            email = "test2@gmail.com",
+        )
+
+        new_data = user_data.model_dump(exclude_unset=True)
 
         users_crud.update_user_data(user, new_data, session)
 
 
 def test_delete_user():
     with db_helper_mock.session_factory() as session:
-        result = users_crud.delete_user_by_id(1, session)
+        result = users_crud.delete_user_by_id(4, session)
 
+def test_invalid_update_user_data():
+    with pytest.raises(ValidationError):
+        with db_helper_mock.session_factory() as session:
+            user = users_crud.get_user_by_id(2, session)
 
+            assert user is not None
+
+            user_data = UserUpdate(
+                name = "Gealor",
+                password = "1"
+            )
+            new_data = user_data.model_dump(exclude_unset=True)
+
+            users_crud.update_user_data(user, new_data, session)
         
