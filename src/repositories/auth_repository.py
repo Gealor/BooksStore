83ff1e68938e58c45aca8f22ae0a74eab8b6e205
Gealor <gzazulya@gmail.com>
2025-07-17 23:33:53 +0400

    create auth repository

---
+++ src/repositories/auth_repository.py
@@ -0,0 +1,30 @@
+from typing import Tuple
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from core.models import User, db_helper
+from core.schemas.users import UserLoginInfo
+
+class AuthRepository:
+    def __init__(self, session: Session):
+        self._session = session
+    
+    def find_user_by_email(
+        self,
+        email : str,
+    ) -> Tuple[str] | None:
+        stmt = select(User.email).where(User.email == email)
+
+        result = self._session.execute(stmt)
+
+        return result.first()
+
+
+    def get_data_by_email(
+        self,
+        username : str,
+    ) -> UserLoginInfo | None:
+        stmt = select(User).where(User.email == username)
+        result = self._session.scalar(stmt)
+
+        return result
