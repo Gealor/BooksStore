72df8d63a3103f2914a1029285eb4159bec97d0f
Gealor <gzazulya@gmail.com>
2025-07-20 16:00:22 +0400

    new tests repositories

--- tests/test_books_crud.py
+++ tests/test_books_crud.py
@@ -4,6 +4,7 @@ from sqlalchemy import text
 from core.schemas.books import BookCreate, BookUpdate
 from crud import books as books_crud
 from core.models.db_helper import db_helper_mock
+from repositories.book_repository import BookRepository
 
 
 def test_create_book():
@@ -15,10 +16,7 @@ def test_create_book():
         number_copies=4,
     )
     with db_helper_mock.session_factory() as session:
-        result = books_crud.create_book(
-            book,
-            session,
-        )
+        result = BookRepository(session=session).create_book(book)
 
         assert result.title == "Билли Саммерс"
         assert result.author == "Стивен Кинг"
@@ -26,42 +24,42 @@ def test_create_book():
 
 def test_get_all_books():
     with db_helper_mock.session_factory() as session:
-        result = books_crud.get_all_books(session)
+        result = BookRepository(session=session).get_all_books()
 
     assert result is not None
 
 
 def test_get_book_by_id():
     with db_helper_mock.session_factory() as session:
-        result = books_crud.get_book_by_id(1, session)
+        result = BookRepository(session=session).get_book_by_id(1)
 
     assert result is not None
 
 
 def test_get_book_by_title():
     with db_helper_mock.session_factory() as session:
-        result = books_crud.get_books_by_name("1984", session)
+        result = BookRepository(session=session).get_books_by_name("1984")
 
     assert result is not None
 
 
 def test_get_book_by_author():
     with db_helper_mock.session_factory() as session:
-        result = books_crud.get_books_by_author("Джордж Оруэлл", session)
+        result = BookRepository(session=session).get_books_by_author("Джордж Оруэлл")
 
     assert result is not None
 
 
 def test_get_book_by_isbn():
     with db_helper_mock.session_factory() as session:
-        result = books_crud.get_books_by_isbn("123456789abcd", session)
+        result = BookRepository(session=session).get_books_by_isbn("123456789abcd")
 
     assert result is not None
 
 
 def test_update_books_by_id():
     with db_helper_mock.session_factory() as session:
-        book = books_crud.get_book_by_id(1, session)
+        book = BookRepository(session=session).get_book_by_id(1)
 
         assert book is not None
 
@@ -71,13 +69,13 @@ def test_update_books_by_id():
         )
         new_data = book_update.model_dump(exclude_unset=True)
 
-        books_crud.update_book_data(book, new_data, session)
+        BookRepository(session=session).update_book_data(book, new_data)
 
 
 def test_invalid_update_book():
     with pytest.raises(ValidationError):
         with db_helper_mock.session_factory() as session:
-            book = books_crud.get_book_by_id(1, session)
+            book = BookRepository(session=session).get_book_by_id(1)
 
             assert book is not None
 
@@ -87,14 +85,18 @@ def test_invalid_update_book():
             )
             new_data = book_update.model_dump(exclude_unset=True)
 
-            books_crud.update_book_data(book, new_data, session)
+            BookRepository(session=session).update_book_data(book, new_data)
 
 
 def test_delete_books():
     with db_helper_mock.session_factory() as session:
-        result = books_crud.delete_book_by_id(1, session)
+        result = BookRepository(session=session).delete_book_by_id(1)
+
+        assert result is not None
 
 
 def test_delete_books_2():
     with db_helper_mock.session_factory() as session:
-        result = books_crud.delete_book_by_id(2, session)
+        result = BookRepository(session=session).delete_book_by_id(2)
+
+        assert result is not None
--- tests/test_users_crud.py
+++ tests/test_users_crud.py
@@ -10,6 +10,7 @@ from sqlalchemy import text
 from core.schemas.users import UserCreate, UserUpdate
 from crud import users as users_crud
 from core.models.db_helper import db_helper_mock
+from repositories.user_repository import UserRepository
 
 
 def test_create_user():
@@ -19,10 +20,7 @@ def test_create_user():
         password="craft123",
     )
     with db_helper_mock.session_factory() as session:
-        result = users_crud.create_user(
-            user,
-            session,
-        )
+        result = UserRepository(session=session).create_user(user)
 
         assert result.name == "Максим"
         assert result.email == "test1@mail.ru"
@@ -30,14 +28,14 @@ def test_create_user():
 
 def test_get_all_users():
     with db_helper_mock.session_factory() as session:
-        result = users_crud.get_all_users(session)
+        result = UserRepository(session=session).get_all_users()
 
     assert result is not None
 
 
 def test_update_user_by_id():
     with db_helper_mock.session_factory() as session:
-        user = users_crud.get_user_by_id(4, session)
+        user = UserRepository(session=session).get_user_by_id(4)
 
         assert user is not None
 
@@ -48,22 +46,22 @@ def test_update_user_by_id():
 
         new_data = user_data.model_dump(exclude_unset=True)
 
-        users_crud.update_user_data(user, new_data, session)
+        UserRepository(session=session).update_user_data(user, new_data)
 
 
 def test_delete_user():
     with db_helper_mock.session_factory() as session:
-        result = users_crud.delete_user_by_id(4, session)
+        result = UserRepository(session=session).delete_user_by_id(4)
 
 
 def test_invalid_update_user_data():
     with pytest.raises(ValidationError):
         with db_helper_mock.session_factory() as session:
-            user = users_crud.get_user_by_id(2, session)
+            user = UserRepository(session=session).get_user_by_id(2)
 
             assert user is not None
 
             user_data = UserUpdate(name="Gealor", password="1")
             new_data = user_data.model_dump(exclude_unset=True)
 
-            users_crud.update_user_data(user, new_data, session)
+            UserRepository(session=session).update_user_data(user, new_data)
