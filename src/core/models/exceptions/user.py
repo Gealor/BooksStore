079e0b09688893d3efa81278d33659d41061e1d0
Gealor <gzazulya@gmail.com>
2025-07-20 16:44:56 +0400

    added user exception

---
+++ src/core/models/exceptions/user.py
@@ -0,0 +1,11 @@
+class BaseUserException(Exception):
+    pass
+
+class UserNotFoundException(BaseUserException):
+    pass
+
+class SelfDeleteException(BaseUserException):
+    pass
+
+class ListUsersNotFoundException(BaseUserException):
+    pass
