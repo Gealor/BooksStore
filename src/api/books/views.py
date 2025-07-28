b29b796d7c9773ba3466860c2021b4ba3a893bfd
Gealor <gzazulya@gmail.com>
2025-07-28 14:57:00 +0400

    refactor books views (endpoints)

--- src/api/books/views.py
+++ src/api/books/views.py
@@ -6,10 +6,11 @@ from sqlalchemy.orm import Session
 from api.auth.tools.tools_auth import get_current_active_auth_user
 from core.config import settings
 from core.models import db_helper
-from core.schemas.exceptions import InvalidDataError
+from core.schemas.exceptions import BookNotFoundException, InvalidDataError
 from core.schemas.users import UserRead
 from crud import books as books_crud
 from core.schemas.books import BookCreate, BookDelete, BookRead, BookUpdate
+from services.book_service import BookService
 
 router = APIRouter(
     prefix=settings.api.books.prefix,
@@ -21,8 +22,7 @@ router = APIRouter(
 def get_all_books(
     session: Annotated[Session, Depends(db_helper.session_getter)],
 ) -> list[BookRead]:
-    books = books_crud.get_all_books(session)
-
+    books = BookService.get_all_books(session)
     return books
 
 
@@ -33,7 +33,7 @@ def create_book(
     user: UserRead = Depends(get_current_active_auth_user),
 ) -> BookRead:
     try:
-        book = books_crud.create_book(book_create, session)
+        book = BookService.create_book(book_create, session)
     except InvalidDataError:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -49,37 +49,32 @@ def update_book_by_id(
     session: Annotated[Session, Depends(db_helper.session_getter)],
     user: UserRead = Depends(get_current_active_auth_user),
 ) -> BookUpdate:
-
-    found_book = books_crud.get_book_by_id(book_id, session)
-    if found_book is None:
-        raise HTTPException(
+    try:
+        result = BookService.update_book_by_id(book_id, book_update, session)
+    except BookNotFoundException as e:
+        HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Book not found",
         )
-
-    values_dict = book_update.model_dump(exclude_unset=True)
-    try:
-        books_crud.update_book_data(found_book, values_dict, session)
     except InvalidDataError:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Invalid data",
         )
-    return book_update
-
-
+    return result
+    
+    
 @router.delete("/delete-book")
 def delete_book_by_id(
     book_id: int,
     session: Annotated[Session, Depends(db_helper.session_getter)],
     user: UserRead = Depends(get_current_active_auth_user),
 ) -> BookDelete:
-    deleted_id = books_crud.delete_book_by_id(book_id, session)
-    if deleted_id is None:
+    try:
+        result = BookService.delete_book_by_id(book_id, session)
+    except BookNotFoundException:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Book not found",
         )
-    return {
-        "deleted": deleted_id,
-    }
+    return result
