5829b5fa1a9e5b644eba11a77c75cd00b6b8cba6
Gealor <gzazulya@gmail.com>
2025-08-02 20:37:29 +0400

    added exception

--- src/core/schemas/exceptions.py
+++ src/core/schemas/exceptions.py
@@ -4,3 +4,27 @@ class InvalidDataError(Exception):
 
 class BookNotFoundException(Exception):
     pass
+
+
+class ZeroCopiesException(Exception):
+    pass
+
+
+class MaxNumberBorrowedBooksException(Exception):
+    pass
+
+
+class ReduceNumberOfCopiesException(Exception):
+    pass
+
+
+class IncreaseNumberOfCopiesException(Exception):
+    pass
+
+
+class UserMissingBookException(Exception):
+    pass
+
+
+class BookAlreadyReturnException(Exception):
+    pass
