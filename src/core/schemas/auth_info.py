5c0135c4325d8433af126eba8de3b92d2f667dfe
Gealor <gzazulya@gmail.com>
2025-08-08 18:44:30 +0400

    added some comments

--- src/core/schemas/auth_info.py
+++ src/core/schemas/auth_info.py
@@ -2,6 +2,6 @@ from pydantic import BaseModel
 
 
 class TokenInfo(BaseModel):
-    access_token: str
+    access_token: str # обязательное, фиксированное имя поля для открытой документации OAuth2PasswordBearer, которая включает схему password flow
     refresh_token: str | None = None
-    token_type: str = "Bearer"
+    token_type: str = "Bearer" # обязательное, фиксированное имя поля для открытой документации OAuth2PasswordBearer, которая включает схему password flow
