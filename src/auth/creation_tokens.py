8ff6e9dbd6826209b4f57d661495b96d748f3d63
Gealor <gzazulya@gmail.com>
2025-09-03 18:57:46 +0400

    move exp and iat to separate variables

--- src/auth/creation_tokens.py
+++ src/auth/creation_tokens.py
@@ -1,5 +1,6 @@
 import copy
 import datetime
+from pathlib import Path
 import jwt
 from core.schemas.users import UserRead
 from core.config import settings
@@ -9,7 +10,7 @@ ACCESS_TOKEN_TYPE = "access"
 ACCESS_TOKEN_TYPE = "access"
 REFRESH_TOKEN_TYPE = "refresh"
 
-
+# Зачем нужен KID в JWT? https://chatgpt.com/c/68a8baa4-88b4-8333-92a5-07e5c76683a9
 def create_jwt(
     token_type: str,
     token_data: dict,
@@ -58,22 +59,25 @@ def encode_jwt(
     algorithm: str = settings.jwt.algorithm,
     expire_minutes: int = settings.jwt.access_token_expire_minutes,
 ):
+    now = datetime.datetime.now(datetime.timezone.utc)
+    exp_token = now + datetime.timedelta(minutes=expire_minutes)
+
     copy_payload = copy.copy(payload)
     # exp - время жизни токена, т.е. указыватся дата, после которой токен сгорает
     copy_payload.update(
         # время для iat и exp надо указывать время по utc, т.е. всемирное координированное время, универсальный мировой стандарт времени.
         # короче без учета часовых поясов
-        exp=datetime.datetime.now(datetime.timezone.utc)
-        + datetime.timedelta(minutes=expire_minutes),
-        iat=datetime.datetime.now(datetime.timezone.utc),
+        exp=exp_token,
+        iat=now,
     )
     encoded = jwt.encode(copy_payload, private_key, algorithm=algorithm)
     return encoded
 
-
 def decode_jwt(
     jwt_token: str | bytes,
     public_key: str = settings.jwt.public_key_path.read_text(),
+    # public_key: str = (Path(__file__).parent / "other_certs" / "jwt-public.pem").read_text(),
+    # будет другая ошибка InvalidTokenError: Signature verification failed, т.е. ошибка подтверждения подписи токена
     algorithm: str = settings.jwt.algorithm,
 ):
     decoded = jwt.decode(
