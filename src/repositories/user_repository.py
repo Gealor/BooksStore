80a4399d1c5a834269f2834da63c5e4948c6a36f
Gealor <gzazulya@gmail.com>
2025-11-06 20:49:03 +0400

    refactor UserRepository

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -1,10 +1,13 @@
+from datetime import datetime
 from typing import Sequence
-from sqlalchemy import select
+from sqlalchemy import select, update
 from sqlalchemy.orm import selectinload, Session
+from sqlalchemy.exc import IntegrityError
 
 
 from auth import hash_password
 from core.models import User
+from core.logger import log
 from core.schemas.users import UserCreate
 
 
@@ -12,16 +15,21 @@ class UserRepository:
     def __init__(self, session: Session):
         self._session = session
 
-    def get_all_users(self) -> Sequence[User]:
+    def get_all_users(self, include_deleted: bool = False) -> Sequence[User]:
         stmt = select(User).order_by(User.id)
+        if not include_deleted:
+            stmt = stmt.where(User.deleted_at.is_(None))
         result = self._session.scalars(stmt)
         return result.all()
 
     def get_user_by_id(
         self,
         user_id: int,
+        include_deleted: bool = False,
     ) -> User | None:
         stmt = select(User).where(User.id == user_id)
+        if not include_deleted:
+            stmt = stmt.where(User.deleted_at.is_(None))
         result = self._session.scalar(stmt)
 
         return result
@@ -31,36 +39,41 @@ class UserRepository:
         user_create: UserCreate,
     ) -> User:
         user_create.password = hash_password(user_create.password).decode("utf-8")
-
-        user = User(**user_create.model_dump())
-        self._session.add(user)
-        self._session.commit()
+        try:
+            user = User(**user_create.model_dump())
+            self._session.add(user)
+        except IntegrityError as e:
+            log.error("Database Exception: %s", e)
+            self._session.rollback()
+        else:
+            self._session.commit()
         return user
 
     def delete_user_by_id(
         self,
         user_id: int,
-    ) -> int | None:
-        stmt = (
-            select(User)
-            .options(selectinload(User.borrowed_books))
-            .where(User.id == user_id)
-        )
-        result = self._session.execute(stmt)
-        user = result.scalar_one_or_none()
-        if user:
-            self._session.delete(user)
-            self._session.commit()
-        return user.id if user else None
+    ) -> datetime | None:
+        stmt = update(User).values(deleted_at=datetime.now()).where(User.id == user_id).returning(User.deleted_at)
+        try:
+            result = self._session.execute(stmt)
+            deleted_at = result.scalar_one_or_none()
+        except IntegrityError as e:
+            log.error("Database Exception: %s", e)
+            self._session.rollback()
+        self._session.commit()
+        return deleted_at
 
     def update_user_data(
         self,
-        user: User,
+        user_id: int,
         new_data: dict,
     ) -> None:
         if "password" in new_data:
             new_data["password"] = hash_password(new_data["password"]).decode("utf-8")
-        for key, value in new_data.items():
-            setattr(user, key, value)
-
+        try:
+            stmt = update(User).values(**new_data).where(User.id == user_id)
+            self._session.execute(stmt)
+        except IntegrityError as e:
+            log.error("Database Exception: %s", e)
+            self._session.rollback()
         self._session.commit()
