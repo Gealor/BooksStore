2dd568c400ce504511d3334cd27a01b090b90827
Gealor <gzazulya@gmail.com>
2025-11-13 01:19:06 +0400

    change readme.md

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
 
 from core.config import settings
+from core.schemas.users import UserBase
 from .auth.auth_router import router as auth_router
 from .users.user_router import router as user_router
 from .books.book_router import router as book_router
 from .business.business_router import router as business_router
 
+from .path_query_body_test import router as random_shit_router
 
 router = APIRouter(
     prefix=settings.api.prefix,
@@ -25,4 +27,9 @@ router.include_router(
 
 router.include_router(
     business_router,
+)
+
+
+router.include_router(
+    random_shit_router,
 )
